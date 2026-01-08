f1b985e4a515a66bdae3d07a8186c332060372c4
Alex | Kronox <alexander_vogt@outlook.de>
2026-01-08 18:45:18 +0100

    ignore some invisible actions in undo

--- frontend/webEditor/src/commandPalette/di.config.ts
+++ frontend/webEditor/src/commandPalette/di.config.ts
@@ -2,10 +2,12 @@ import { ContainerModule } from "inversify";
 import { CommandPalette, TYPES } from "sprotty";
 import { WebEditorCommandPaletteActionProvider } from "./commandPaletteProvider";
 import { WebEditorCommandPalette } from "./commandPalette";
+import { DiagramModificationCommandStack } from "./commandStack";
 
 export const commandPaletteModule = new ContainerModule((bind, _, __, rebind) => {
     rebind(CommandPalette).to(WebEditorCommandPalette).inSingletonScope();
 
     bind(WebEditorCommandPaletteActionProvider).toSelf().inSingletonScope();
     bind(TYPES.ICommandPaletteActionProvider).toService(WebEditorCommandPaletteActionProvider);
+    rebind(TYPES.ICommandStack).to(DiagramModificationCommandStack).inSingletonScope();
 });
