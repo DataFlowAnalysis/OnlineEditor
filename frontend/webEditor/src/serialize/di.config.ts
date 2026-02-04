ff08cb50495bc75c23dcc4435cd5a15d38c11d45
Alex | Kronox <alexander_vogt@outlook.de>
2026-02-04 12:52:20 +0100

    readd image export

--- frontend/webEditor/src/serialize/di.config.ts
+++ frontend/webEditor/src/serialize/di.config.ts
@@ -9,6 +9,7 @@ import { SaveJsonFileCommand } from "./saveJsonFile";
 import { SaveDfdAndDdFileCommand } from "./saveDfdAndDdFile";
 import { AnalyzeCommand } from "./analyze";
 import { LoadFromUrlCommand } from "./LoadUrl";
+import { SaveImageCommand } from "./image";
 import { JsonDropHandler, LoadDroppedFileCommand } from "./dropListener";
 
 export const serializeModule = new ContainerModule((bind, unbind, isBound, rebind) => {
@@ -21,6 +22,7 @@ export const serializeModule = new ContainerModule((bind, unbind, isBound, rebin
     configureCommand(context, SaveJsonFileCommand);
     configureCommand(context, SaveDfdAndDdFileCommand);
     configureCommand(context, AnalyzeCommand);
+    configureCommand(context, SaveImageCommand);
     configureCommand(context, LoadDroppedFileCommand);
 
     bind(TYPES.MouseListener).to(JsonDropHandler);
