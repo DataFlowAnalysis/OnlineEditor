e5a58089273a7f2a35203b4ee7e787fc0876d8b6
Alex | Kronox <alexander_vogt@outlook.de>
2026-02-06 13:30:45 +0100

    fix build

--- frontend/webEditor/src/serialize/di.config.ts
+++ frontend/webEditor/src/serialize/di.config.ts
@@ -9,7 +9,7 @@ import { SaveJsonFileCommand } from "./saveJsonFile";
 import { SaveDfdAndDdFileCommand } from "./saveDfdAndDdFile";
 import { AnalyzeCommand } from "./analyze";
 import { LoadFromUrlCommand } from "./LoadUrl";
-import { SaveImageCommand } from "./export";
+import { ExportCommand } from "./export";
 
 export const serializeModule = new ContainerModule((bind, unbind, isBound, rebind) => {
     const context = { bind, unbind, isBound, rebind };
@@ -21,7 +21,7 @@ export const serializeModule = new ContainerModule((bind, unbind, isBound, rebin
     configureCommand(context, SaveJsonFileCommand);
     configureCommand(context, SaveDfdAndDdFileCommand);
     configureCommand(context, AnalyzeCommand);
-    configureCommand(context, SaveImageCommand);
+    configureCommand(context, ExportCommand);
 
     rebind(TYPES.IModelFactory).to(DfdModelFactory);
 });
