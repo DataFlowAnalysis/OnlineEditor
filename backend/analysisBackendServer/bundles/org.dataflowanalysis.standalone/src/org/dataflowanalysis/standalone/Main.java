9cc60b2086e475db828bbd01153025ddd2091f32
dalu-wins <dalu-wins@users.noreply.github.com>
2026-03-09 21:36:53 +0100

    fix: remove log version

--- backend/analysisBackendServer/bundles/org.dataflowanalysis.standalone/src/org/dataflowanalysis/standalone/Main.java
+++ backend/analysisBackendServer/bundles/org.dataflowanalysis.standalone/src/org/dataflowanalysis/standalone/Main.java
@@ -7,7 +7,6 @@ public class Main {
 	public static void main(String[] args) {		
 		try {			       
             Thread webSocketServer =  WebSocketServerUtils.startWebSocketServer();
-            DataFlowConfidentialityAnalysis.logVersion();
                         
             while(webSocketServer.isAlive());
 
