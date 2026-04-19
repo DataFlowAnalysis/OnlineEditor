8e00d3e2b42764a9a2d40c89a0bf57101508f113
Alex | Kronox <alexander_vogt@outlook.de>
2026-04-19 13:43:09 +0200

    test tokenization

---
+++ frontend/webEditor/tests/languages/tokenize.test.ts
@@ -0,0 +1,51 @@
+import { expect, it } from "vitest";
+import { Token, tokenize } from "../../src/languages/tokenize";
+
+it("Tokenization", () => {
+    for (const testSet of TEST_DATA) {
+        const result = tokenize(testSet.input);
+        expect(result.length, `Unexpected Token length for input "${testSet.input.join("\\n")}"`).toBe(
+            testSet.expected.length,
+        );
+        for (let i = 0; i < testSet.expected.length; i++) {
+            const keysToTest = Object.keys(testSet.expected[i]) as (keyof Token)[];
+            for (const key of keysToTest) {
+                expect(
+                    result[i][key],
+                    `Property of token ${i} with key "${key}" for input "${testSet.input.join("\\n")}" did not match`,
+                ).toBe(testSet.expected[i][key]);
+            }
+        }
+    }
+});
+
+const TEST_DATA: TokenizationTestData[] = [
+    {
+        input: ["Hello World"],
+        expected: [
+            { line: 1, column: 1, text: "Hello" },
+            { line: 1, column: 7, text: "World" },
+        ],
+    },
+    {
+        input: ["Long     Gap"],
+        expected: [{ column: 1 }, { column: 10 }],
+    },
+    {
+        input: ["Multi", "Line"],
+        expected: [
+            { line: 1, column: 1 },
+            { line: 2, column: 1 },
+        ],
+    },
+    {
+        input: ["Gap at", "    line start"],
+        expected: [{ line: 1, column: 1 }, { line: 1 }, { line: 2, column: 5 }, { line: 2, column: 10 }],
+    },
+];
+
+interface TokenizationTestData {
+    input: string[];
+    /** For each token you only need to give the parameters you want to test for (e.g. only correct line). If you just test for the token being present an empty object can be given. */
+    expected: Partial<Token>[];
+}
