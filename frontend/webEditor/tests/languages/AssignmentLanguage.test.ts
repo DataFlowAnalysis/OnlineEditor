96440ebfb2b2be57d40af339256da019414b34ec
Alexander Vogt <alexander_vogt@outlook.de>
2026-04-28 13:19:35 +0200

    simple test for assignment language

--- frontend/webEditor/tests/languages/AssignmentLanguage.test.ts
+++ frontend/webEditor/tests/languages/AssignmentLanguage.test.ts
@@ -10,15 +10,58 @@ import {
     ReplacementTestData,
 } from "./LanguageTestUtils";
 
-const VALID_EXPRESSIONS: ValidExpressionTestData[] = [["forward flow"], ["forward hello|world"]];
+const BASE_VALID_EXPRESSIONS: ValidExpressionTestData[] = [
+    ["forward flow"],
+    ["forward hello|world"],
+    ["set Sensitivity.Public"],
+    ["unset Location.nonEU"],
+    ["assign Sensitivity.Public if TRUE from flow"],
+    ["assign Sensitivity.Public if TRUE"],
+    ["assign Sensitivity.Public if flow.Sensitivity.Public"],
+];
 
-const INVALID_EXPRESSIONS: InvalidExpressionTestData[] = [];
+const VALID_EXPRESSIONS = [...BASE_VALID_EXPRESSIONS, BASE_VALID_EXPRESSIONS.flat()];
 
-const AUTOCOMPLETE_TEST_DATA: AutoCompleteTestData[] = [];
+const INVALID_EXPRESSIONS: InvalidExpressionTestData[] = [
+    { input: ["forward Sensitivity.Public"] },
+    { input: ["set flow"] },
+    { input: ["forward hello"] },
+    { input: ["set Sensitivity.public"] },
+    { input: ["set sensitivity.Public"] },
+];
 
-const REPLACEMENT_TEST_DATA: ReplacementTestData[] = [];
+const AUTOCOMPLETE_TEST_DATA: AutoCompleteTestData[] = [
+    { input: [""], completionOptions: ["forward", "set", "unset", "assign"], exactOptionCount: true },
+    { input: ["forward "], completionOptions: ["flow", "hello|world"], exactOptionCount: true },
+    { input: ["set "], completionOptions: ["Sensitivity", "Location"], exactOptionCount: true },
+    { input: ["set Sensitivity."], completionOptions: ["Personal", "Public"], exactOptionCount: true },
+    { input: ["unset "], completionOptions: ["Sensitivity", "Location"], exactOptionCount: true },
+    { input: ["assign "], completionOptions: ["Sensitivity", "Location"] },
+    { input: ["assign Sensitivity.Public "], completionOptions: ["if"], exactOptionCount: true },
+    { input: ["assign Sensitivity.Public if TRUE "], completionOptions: ["from"] },
+];
+
+const REPLACEMENT_TEST_DATA: ReplacementTestData[] = [
+    {
+        input: ["set Sensitivity.Public"],
+        replacement: { old: "Sensitivity.Public", replacement: "Sensitivity.New", type: "label" },
+        output: ["set Sensitivity.New"],
+    },
+];
 
 const labelTypeRegistry = new LabelTypeRegistry();
+const location = labelTypeRegistry.registerLabelType("Location");
+for (const defaultValue of location.values) {
+    labelTypeRegistry.unregisterLabelTypeValue(location.id, defaultValue.id);
+}
+labelTypeRegistry.registerLabelTypeValue(location.id, "EU");
+labelTypeRegistry.registerLabelTypeValue(location.id, "nonEU");
+const sensitivity = labelTypeRegistry.registerLabelType("Sensitivity");
+for (const defaultValue of sensitivity.values) {
+    labelTypeRegistry.unregisterLabelTypeValue(sensitivity.id, defaultValue.id);
+}
+labelTypeRegistry.registerLabelTypeValue(sensitivity.id, "Personal");
+labelTypeRegistry.registerLabelTypeValue(sensitivity.id, "Public");
 
 const mockPort = {
     // @ts-expect-error We mock the abstract class into a concrete one for simplicity. TypeScript however still thinks this is an abstract class, so it would throw an error
@@ -26,7 +69,7 @@ const mockPort = {
 } as unknown as DfdOutputPortImpl;
 
 generateTests(
-    "Functionality Test Language",
+    "Assignment Language",
     AssignmentLanguageTreeBuilder.buildTree(mockPort, labelTypeRegistry),
     VALID_EXPRESSIONS,
     INVALID_EXPRESSIONS,
