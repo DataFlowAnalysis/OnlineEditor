b5247688ac41a240272350b0379acb89eb8696a9
Alex | Kronox <alexander_vogt@outlook.de>
2026-06-27 17:15:40 +0200

    focus sprotty root

--- frontend/webEditor/src/startUpAgent/di.config.ts
+++ frontend/webEditor/src/startUpAgent/di.config.ts
@@ -3,9 +3,13 @@ import { StartUpAgent } from "./StartUpAgent";
 import { LoadDefaultUiExtensionsStartUpAgent } from "./LoadDefaultUiExtensions";
 import { LoadDefaultDiagramStartUpAgent } from "./LoadDefaultDiagram";
 import { SettingsInitStartUpAgent } from "./settingsInit";
+import { FocusContainerCommand, FocusContainerStartUpAgent } from "./FocusContainer";
+import { configureCommand } from "sprotty";
 
-export const startUpAgentModule = new ContainerModule((bind) => {
+export const startUpAgentModule = new ContainerModule((bind, _, isBound) => {
     bind(StartUpAgent).to(LoadDefaultUiExtensionsStartUpAgent);
     bind(StartUpAgent).to(LoadDefaultDiagramStartUpAgent);
     bind(StartUpAgent).to(SettingsInitStartUpAgent);
+    bind(StartUpAgent).to(FocusContainerStartUpAgent);
+    configureCommand({ bind, isBound }, FocusContainerCommand);
 });
